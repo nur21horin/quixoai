f8ac1a5dcabeebd419c5dc005477b8d30a3819b3
Nur Mohammod <nurm98371@gmail.com>
2026-04-14 13:20:21 +0600

    ADD

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Navbar = () => {
-    
+    const handleAuthClick = () => {
+        // Implement your authentication logic here
+        console.log('Authentication button clicked');
+    }
   return (
     <div className='navbar'>
       <nav className='inner'>
